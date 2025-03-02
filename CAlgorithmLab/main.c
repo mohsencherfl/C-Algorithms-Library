07052fa552e2bc7a0889e2abde38c16269a8d8ea
mohsen bashashi <45238123+mohsencherfl@users.noreply.github.com>
2025-03-02 23:14:02 +0330

    add new function: "finding_next_day"
    There are no input validations in this function for the time being, so users must be sure on their own that they have entered the correct numbers for "year", "month", and "day"

--- CAlgorithmLab/main.c
+++ CAlgorithmLab/main.c
@@ -17,6 +17,7 @@
     void calculate_travel_time();
     void divisible_or_indivisible();
     void finding_leap_year_gregorian_calendar();
+    void finding_next_day();
 
 
 int main()
@@ -36,6 +37,7 @@ int main()
      calculate_travel_time();
      divisible_or_indivisible();
      finding_leap_year_gregorian_calendar();
+     finding_next_day();
 }
 
 
@@ -372,4 +374,66 @@ void finding_leap_year_gregorian_calendar() {
         printf("Not a leap year \n");
 
 }
+
+void finding_next_day() {
+    int year, month, day;
+    int leapYear = 0;
+
+    printf("Enter the year: ");
+    scanf("%d", &year);
+    printf("Enter the month: ");
+    scanf("%d", &month);
+    printf("Enter the day: ");
+    scanf("%d", &day);
+
+    if(month == 2)
+        if(year % 4 == 0 && year % 100 != 0 || year % 400 == 0)
+        leapYear = 1;
+
+    day++;
+
+    switch(month) {
+
+    case 1:
+    case 3:
+    case 5:
+    case 7:
+    case 8:
+    case 10:
+    case 12:
+        if(day <= 31)
+            break;
+    case 4:
+    case 6:
+    case 9:
+    case 11:
+        if(day <= 30)
+            break;
+    case 2:
+        if(day <= 29 && leapYear == 1)
+            break;
+        else if(day <= 28)
+            break;
+    default:
+        day = 1;
+        month++;
+        if(month > 12) {
+            month = 1;
+            year++;
+        }
+        printf("%d-%02d-%02d \n", year, month, day);
+    }
+
+}
+
+
+
+
+
+
+
+
+
+
+
 
